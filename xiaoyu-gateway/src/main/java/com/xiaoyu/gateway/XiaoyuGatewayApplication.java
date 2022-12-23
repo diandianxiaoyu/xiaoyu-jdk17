9d76a6e5d3572f82740d283a416cc004f99c7371
gyj <1425858726@qq.com>
2022-12-23 16:20:22 +0800

    init

---
+++ xiaoyu-gateway/src/main/java/com/xiaoyu/gateway/XiaoyuGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoyuGatewayApplication.class, args);
+    }
+
+}
--- xiaoyu-gateway/src/test/java/com/xiaoyu/gateway/XiaoyuGatewayApplicationTests.java
+++ xiaoyu-gateway/src/test/java/com/xiaoyu/gateway/XiaoyuGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.xiaoyu.common;
+package com.xiaoyu.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class XiaoyuCommonApplicationTests {
+class XiaoyuGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
